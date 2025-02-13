0721fbcf19d3fbbd8faae7313ecb775973229f91
Martin Vahlensieck <martin.vahlensieck@unibas.ch>
2025-02-13 11:41:24 +0100

    Expand deadlock check

--- src/com/oltpbenchmark/api/Worker.java
+++ src/com/oltpbenchmark/api/Worker.java
@@ -495,7 +495,7 @@ public abstract class Worker<T extends BenchmarkModule> implements Runnable {
                     // ------------------
                     // Polypheny
                     // ------------------
-                    } else if (ex.getMessage().contains( "DeadlockException" ) || ex.getMessage().contains("Could not acquire lock") || ex.getMessage().contains("Write-write")) {
+                    } else if (ex.getMessage().contains( "DeadlockException" ) || ex.getMessage().contains( "deadlock" ) || ex.getMessage().contains("Could not acquire lock") || ex.getMessage().contains("Write-write")) {
                         // Polypheny Deadlock
                         status = TransactionStatus.RETRY;
                         continue;
